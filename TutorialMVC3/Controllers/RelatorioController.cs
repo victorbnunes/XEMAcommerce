4daa8e16eab87cac4838178b3e7bd6ad2d3b535b
Victor <victor.bnunes@gmail.com>
2013-01-05 17:11:07 -0200

    555

---
+++ TutorialMVC3/Controllers/RelatorioController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TutorialMVC3.Controllers
+{
+    public class RelatorioController : Controller
+    {
+        //
+        // GET: /Relatorio/
+
+        public ActionResult Relatorio()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ TutorialMVC3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TutorialMVC3.MvcApplication" Language="C#" %>
